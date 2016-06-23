c95b7722ad014354ecfcad52f169ed11f1156cf0
nikhil.rayaprolu <nikhil.rayap@gmail.com>
2016-06-23 15:57:08 +0530

    kaushik take it

---
+++ public/controllers/test2.js
@@ -0,0 +1,10 @@
+var app=angular.module('Ieducative',['ngRoute']);
+app.controller('TestingController',['$location','$scope','$http','$routeParams',function($location,$scope,$http,$routeParams){
+	
+
+}]);
+app.config(function($locationProvider) {
+ $locationProvider.html5Mode(true); 
+});
+
+
